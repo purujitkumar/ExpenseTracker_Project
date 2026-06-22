5dd62baa5dc630ed9fa26c321733b541ac8bc8bf
purujitkumar <purujitkumar2@gmail.com>
2026-06-23 03:37:29 +0530

    Implemented basic app window.

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,1 +1,11 @@
-print("HelloWorld!")
+import customtkinter as ctk
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("green")
+class App(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("App")    
+        self.geometry("1366x768")
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
